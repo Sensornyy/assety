aa7a1db5dbe06014d0b486774cf6e203e8032ee8
Maksym Koriak <m.koriak@sharksw.com>
2025-05-13 12:15:06 +0300

    refactor: clean code

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:assety/features/auth/presentation/bloc/auth_bloc.dart';
-import 'package:assety/features/auth/presentation/screen/auth_screen.dart';
-import 'package:assety/features/investments/crypto/presentation/screens/add_crypto_screen.dart';
+import 'package:assety/features/transactions/presentation/screens/transactions_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,10 +8,44 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AuthBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthBloc(),
+        )
+      ],
       child: MaterialApp(
-        home: AuthScreen(),
+        theme: ThemeData(
+          textTheme: TextTheme(
+            bodyMedium: TextStyle(fontSize: 18),
+            labelSmall: TextStyle(fontSize: 16),
+            labelMedium: TextStyle(fontSize: 16),
+            labelLarge: TextStyle(fontSize: 16),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.deepPurpleAccent),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.deepPurpleAccent),
+            ),
+            disabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.deepPurpleAccent),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.deepPurpleAccent),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.deepPurpleAccent),
+            ),
+          ),
+        ),
+        home: TransactionsScreen(),
       ),
     );
   }
--- lib/features/investments/crypto/domain/repository/crypto_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:assety/core/data/error_handler/result.dart';
-import 'package:assety/features/investments/crypto/domain/entity/token_entity.dart';
-
-abstract class CryptoRepository {
-  Future<Result<List<TokenEntity>>> getTop100Tokens();
-
-  Future<Result<TokenEntity>> getToken(String symbol);
-}
